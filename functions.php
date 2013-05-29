a0ed30ee43331feebfe421965b63b54026e84009
shrakner <shrakner@gmail.com>
2013-05-29 08:01:08 -0600

    Added intersecting lines post styling, new compact date display, and new widget list style.

--- functions.php
+++ functions.php
@@ -26,7 +26,7 @@
  * Set the content width based on the theme's design and stylesheet.
  */
 if ( ! isset( $content_width ) )
-	$content_width = 640; /* pixels */
+    $content_width = 640; /* pixels */
 
 if ( ! function_exists( 'toolbox_setup' ) ):
 /**
@@ -40,35 +40,35 @@ if ( ! function_exists( 'toolbox_setup' ) ):
  * functions.php file.
  */
 function toolbox_setup() {
-	/**
-	 * Make theme available for translation
-	 * Translations can be filed in the /languages/ directory
-	 * If you're building a theme based on toolbox, use a find and replace
-	 * to change 'toolbox' to the name of your theme in all the template files
-	 */
-	load_theme_textdomain( 'toolbox', get_template_directory() . '/languages' );
+    /**
+     * Make theme available for translation
+     * Translations can be filed in the /languages/ directory
+     * If you're building a theme based on toolbox, use a find and replace
+     * to change 'toolbox' to the name of your theme in all the template files
+     */
+    load_theme_textdomain( 'toolbox', get_template_directory() . '/languages' );
 
-	$locale = get_locale();
-	$locale_file = get_template_directory() . "/languages/$locale.php";
-	if ( is_readable( $locale_file ) )
-		require_once( $locale_file );
+    $locale = get_locale();
+    $locale_file = get_template_directory() . "/languages/$locale.php";
+    if ( is_readable( $locale_file ) )
+        require_once( $locale_file );
 
-	/**
-	 * Add default posts and comments RSS feed links to head
-	 */
-	add_theme_support( 'automatic-feed-links' );
+    /**
+     * Add default posts and comments RSS feed links to head
+     */
+    add_theme_support( 'automatic-feed-links' );
 
-	/**
-	 * This theme uses wp_nav_menu() in one location.
-	 */
-	register_nav_menus( array(
-		'primary' => __( 'Primary Menu', 'toolbox' ),
-	) );
+    /**
+     * This theme uses wp_nav_menu() in one location.
+     */
+    register_nav_menus( array(
+        'primary' => __( 'Primary Menu', 'toolbox' ),
+    ) );
 
-	/**
-	 * Add support for the Aside and Gallery Post Formats
-	 */
-	add_theme_support( 'post-formats', array( 'aside', 'image', 'gallery' ) );
+    /**
+     * Add support for the Aside and Gallery Post Formats
+     */
+    add_theme_support( 'post-formats', array( 'aside', 'image', 'gallery' ) );
 }
 endif; // toolbox_setup
 
@@ -81,17 +81,17 @@ add_action( 'after_setup_theme', 'toolbox_setup' );
  * Set a default theme color array for WP.com.
  */
 $themecolors = array(
-	'bg' => 'ffffff',
-	'border' => 'eeeeee',
-	'text' => '444444',
+    'bg' => 'ffffff',
+    'border' => 'eeeeee',
+    'text' => '444444',
 );
 
 /**
  * Get our wp_nav_menu() fallback, wp_page_menu(), to show a home link.
  */
 function toolbox_page_menu_args( $args ) {
-	$args['show_home'] = true;
-	return $args;
+    $args['show_home'] = true;
+    return $args;
 }
 add_filter( 'wp_page_menu_args', 'toolbox_page_menu_args' );
 
@@ -99,24 +99,24 @@ add_filter( 'wp_page_menu_args', 'toolbox_page_menu_args' );
  * Register widgetized area and update sidebar with default widgets
  */
 function toolbox_widgets_init() {
-	register_sidebar( array(
-		'name' => __( 'Sidebar 1', 'toolbox' ),
-		'id' => 'sidebar-1',
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget' => "</aside>",
-		'before_title' => '<h1 class="widget-title">',
-		'after_title' => '</h1>',
-	) );
+    register_sidebar( array(
+        'name' => __( 'Sidebar 1', 'toolbox' ),
+        'id' => 'sidebar-1',
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget' => "</aside>",
+        'before_title' => '<h1 class="widget-title">',
+        'after_title' => '</h1>',
+    ) );
 
-	register_sidebar( array(
-		'name' => __( 'Sidebar 2', 'toolbox' ),
-		'id' => 'sidebar-2',
-		'description' => __( 'An optional second sidebar area', 'toolbox' ),
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget' => "</aside>",
-		'before_title' => '<h1 class="widget-title">',
-		'after_title' => '</h1>',
-	) );
+    register_sidebar( array(
+        'name' => __( 'Sidebar 2', 'toolbox' ),
+        'id' => 'sidebar-2',
+        'description' => __( 'An optional second sidebar area', 'toolbox' ),
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget' => "</aside>",
+        'before_title' => '<h1 class="widget-title">',
+        'after_title' => '</h1>',
+    ) );
 }
 add_action( 'init', 'toolbox_widgets_init' );
 
@@ -127,31 +127,31 @@ if ( ! function_exists( 'toolbox_content_nav' ) ):
  * @since Toolbox 1.2
  */
 function toolbox_content_nav( $nav_id ) {
-	global $wp_query;
+    global $wp_query;
 
-	?>
-	<nav id="<?php echo $nav_id; ?>">
-		<h1 class="assistive-text section-heading"><?php _e( 'Post navigation', 'toolbox' ); ?></h1>
+    ?>
+    <nav id="<?php echo $nav_id; ?>">
+        <h1 class="assistive-text section-heading"><?php _e( 'Post navigation', 'toolbox' ); ?></h1>
 
-	<?php if ( is_single() ) : // navigation links for single posts ?>
+    <?php if ( is_single() ) : // navigation links for single posts ?>
 
-		<?php previous_post_link( '<div class="nav-previous">%link</div>', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'toolbox' ) . '</span> %title' ); ?>
-		<?php next_post_link( '<div class="nav-next">%link</div>', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'toolbox' ) . '</span>' ); ?>
+        <?php previous_post_link( '<div class="nav-previous">%link</div>', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'toolbox' ) . '</span> %title' ); ?>
+        <?php next_post_link( '<div class="nav-next">%link</div>', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'toolbox' ) . '</span>' ); ?>
 
-	<?php elseif ( $wp_query->max_num_pages > 1 && ( is_home() || is_archive() || is_search() ) ) : // navigation links for home, archive, and search pages ?>
+    <?php elseif ( $wp_query->max_num_pages > 1 && ( is_home() || is_archive() || is_search() ) ) : // navigation links for home, archive, and search pages ?>
 
-		<?php if ( get_next_posts_link() ) : ?>
-		<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'toolbox' ) ); ?></div>
-		<?php endif; ?>
+        <?php if ( get_next_posts_link() ) : ?>
+        <div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'toolbox' ) ); ?></div>
+        <?php endif; ?>
 
-		<?php if ( get_previous_posts_link() ) : ?>
-		<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'toolbox' ) ); ?></div>
-		<?php endif; ?>
+        <?php if ( get_previous_posts_link() ) : ?>
+        <div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'toolbox' ) ); ?></div>
+        <?php endif; ?>
 
-	<?php endif; ?>
+    <?php endif; ?>
 
-	</nav><!-- #<?php echo $nav_id; ?> -->
-	<?php
+    </nav><!-- #<?php echo $nav_id; ?> -->
+    <?php
 }
 endif; // toolbox_content_nav
 
@@ -168,50 +168,50 @@ if ( ! function_exists( 'toolbox_comment' ) ) :
  * @since Toolbox 0.4
  */
 function toolbox_comment( $comment, $args, $depth ) {
-	$GLOBALS['comment'] = $comment;
-	switch ( $comment->comment_type ) :
-		case 'pingback' :
-		case 'trackback' :
-	?>
-	<li class="post pingback">
-		<p><?php _e( 'Pingback:', 'toolbox' ); ?> <?php comment_author_link(); ?><?php edit_comment_link( __( '(Edit)', 'toolbox' ), ' ' ); ?></p>
-	<?php
-			break;
-		default :
-	?>
-	<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
-		<article id="comment-<?php comment_ID(); ?>" class="comment">
-			<footer>
-				<div class="comment-author vcard">
-					<?php echo get_avatar( $comment, 40 ); ?>
-					<?php printf( __( '%s <span class="says">says:</span>', 'toolbox' ), sprintf( '<cite class="fn">%s</cite>', get_comment_author_link() ) ); ?>
-				</div><!-- .comment-author .vcard -->
-				<?php if ( $comment->comment_approved == '0' ) : ?>
-					<em><?php _e( 'Your comment is awaiting moderation.', 'toolbox' ); ?></em>
-					<br />
-				<?php endif; ?>
+    $GLOBALS['comment'] = $comment;
+    switch ( $comment->comment_type ) :
+        case 'pingback' :
+        case 'trackback' :
+    ?>
+    <li class="post pingback">
+        <p><?php _e( 'Pingback:', 'toolbox' ); ?> <?php comment_author_link(); ?><?php edit_comment_link( __( '(Edit)', 'toolbox' ), ' ' ); ?></p>
+    <?php
+            break;
+        default :
+    ?>
+    <li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
+        <article id="comment-<?php comment_ID(); ?>" class="comment">
+            <footer>
+                <div class="comment-author vcard">
+                    <?php echo get_avatar( $comment, 40 ); ?>
+                    <?php printf( __( '%s <span class="says">says:</span>', 'toolbox' ), sprintf( '<cite class="fn">%s</cite>', get_comment_author_link() ) ); ?>
+                </div><!-- .comment-author .vcard -->
+                <?php if ( $comment->comment_approved == '0' ) : ?>
+                    <em><?php _e( 'Your comment is awaiting moderation.', 'toolbox' ); ?></em>
+                    <br />
+                <?php endif; ?>
 
-				<div class="comment-meta commentmetadata">
-					<a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>"><time pubdate datetime="<?php comment_time( 'c' ); ?>">
-					<?php
-						/* translators: 1: date, 2: time */
-						printf( __( '%1$s at %2$s', 'toolbox' ), get_comment_date(), get_comment_time() ); ?>
-					</time></a>
-					<?php edit_comment_link( __( '(Edit)', 'toolbox' ), ' ' );
-					?>
-				</div><!-- .comment-meta .commentmetadata -->
-			</footer>
+                <div class="comment-meta commentmetadata">
+                    <a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>"><time pubdate datetime="<?php comment_time( 'c' ); ?>">
+                    <?php
+                        /* translators: 1: date, 2: time */
+                        printf( __( '%1$s at %2$s', 'toolbox' ), get_comment_date(), get_comment_time() ); ?>
+                    </time></a>
+                    <?php edit_comment_link( __( '(Edit)', 'toolbox' ), ' ' );
+                    ?>
+                </div><!-- .comment-meta .commentmetadata -->
+            </footer>
 
-			<div class="comment-content"><?php comment_text(); ?></div>
+            <div class="comment-content"><?php comment_text(); ?></div>
 
-			<div class="reply">
-				<?php comment_reply_link( array_merge( $args, array( 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
-			</div><!-- .reply -->
-		</article><!-- #comment-## -->
+            <div class="reply">
+                <?php comment_reply_link( array_merge( $args, array( 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
+            </div><!-- .reply -->
+        </article><!-- #comment-## -->
 
-	<?php
-			break;
-	endswitch;
+    <?php
+            break;
+    endswitch;
 }
 endif; // ends check for toolbox_comment()
 
@@ -223,15 +223,15 @@ if ( ! function_exists( 'toolbox_posted_on' ) ) :
  * @since Toolbox 1.2
  */
 function toolbox_posted_on() {
-	printf( __( '<span class="sep">Posted on </span><a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s" pubdate>%4$s</time></a><span class="byline"> <span class="sep"> by </span> <span class="author vcard"><a class="url fn n" href="%5$s" title="%6$s" rel="author">%7$s</a></span></span>', 'toolbox' ),
-		esc_url( get_permalink() ),
-		esc_attr( get_the_time() ),
-		esc_attr( get_the_date( 'c' ) ),
-		esc_html( get_the_date() ),
-		esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
-		esc_attr( sprintf( __( 'View all posts by %s', 'toolbox' ), get_the_author() ) ),
-		esc_html( get_the_author() )
-	);
+    printf( __( '<a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s" pubdate>%4$s</time></a><span class="byline"> <span class="sep"> by </span> <span class="author vcard"><a class="url fn n" href="%5$s" title="%6$s" rel="author">%7$s</a></span></span>', 'toolbox' ),
+        esc_url( get_permalink() ),
+        esc_attr( get_the_time() ),
+        esc_attr( get_the_date( 'c' ) ),
+        '<span class="date-month">' . esc_html( get_the_date( 'M' )  ). '</span><span class="date-day">' . esc_html( get_the_date( 'd' ) ) . '</span><span class="date-year">' . esc_html( get_the_date( 'Y' ) ) . '</span',
+        esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+        esc_attr( sprintf( __( 'View all posts by %s', 'toolbox' ), get_the_author() ) ),
+        esc_html( get_the_author() )
+    );
 }
 endif;
 
@@ -241,12 +241,12 @@ endif;
  * @since Toolbox 1.2
  */
 function toolbox_body_classes( $classes ) {
-	// Adds a class of single-author to blogs with only 1 published author
-	if ( ! is_multi_author() ) {
-		$classes[] = 'single-author';
-	}
+    // Adds a class of single-author to blogs with only 1 published author
+    if ( ! is_multi_author() ) {
+        $classes[] = 'single-author';
+    }
 
-	return $classes;
+    return $classes;
 }
 add_filter( 'body_class', 'toolbox_body_classes' );
 
@@ -256,25 +256,25 @@ add_filter( 'body_class', 'toolbox_body_classes' );
  * @since Toolbox 1.2
  */
 function toolbox_categorized_blog() {
-	if ( false === ( $all_the_cool_cats = get_transient( 'all_the_cool_cats' ) ) ) {
-		// Create an array of all the categories that are attached to posts
-		$all_the_cool_cats = get_categories( array(
-			'hide_empty' => 1,
-		) );
+    if ( false === ( $all_the_cool_cats = get_transient( 'all_the_cool_cats' ) ) ) {
+        // Create an array of all the categories that are attached to posts
+        $all_the_cool_cats = get_categories( array(
+            'hide_empty' => 1,
+        ) );
 
-		// Count the number of categories that are attached to the posts
-		$all_the_cool_cats = count( $all_the_cool_cats );
+        // Count the number of categories that are attached to the posts
+        $all_the_cool_cats = count( $all_the_cool_cats );
 
-		set_transient( 'all_the_cool_cats', $all_the_cool_cats );
-	}
+        set_transient( 'all_the_cool_cats', $all_the_cool_cats );
+    }
 
-	if ( '1' != $all_the_cool_cats ) {
-		// This blog has more than 1 category so toolbox_categorized_blog should return true
-		return true;
-	} else {
-		// This blog has only 1 category so toolbox_categorized_blog should return false
-		return false;
-	}
+    if ( '1' != $all_the_cool_cats ) {
+        // This blog has more than 1 category so toolbox_categorized_blog should return true
+        return true;
+    } else {
+        // This blog has only 1 category so toolbox_categorized_blog should return false
+        return false;
+    }
 }
 
 /**
@@ -283,8 +283,8 @@ function toolbox_categorized_blog() {
  * @since Toolbox 1.2
  */
 function toolbox_category_transient_flusher() {
-	// Like, beat it. Dig?
-	delete_transient( 'all_the_cool_cats' );
+    // Like, beat it. Dig?
+    delete_transient( 'all_the_cool_cats' );
 }
 add_action( 'edit_category', 'toolbox_category_transient_flusher' );
 add_action( 'save_post', 'toolbox_category_transient_flusher' );
@@ -293,14 +293,14 @@ add_action( 'save_post', 'toolbox_category_transient_flusher' );
  * Filter in a link to a content ID attribute for the next/previous image links on image attachment pages
  */
 function toolbox_enhanced_image_navigation( $url ) {
-	global $post, $wp_rewrite;
+    global $post, $wp_rewrite;
 
-	$id = (int) $post->ID;
-	$object = get_post( $id );
-	if ( wp_attachment_is_image( $post->ID ) && ( $wp_rewrite->using_permalinks() && ( $object->post_parent > 0 ) && ( $object->post_parent != $id ) ) )
-		$url = $url . '#main';
+    $id = (int) $post->ID;
+    $object = get_post( $id );
+    if ( wp_attachment_is_image( $post->ID ) && ( $wp_rewrite->using_permalinks() && ( $object->post_parent > 0 ) && ( $object->post_parent != $id ) ) )
+        $url = $url . '#main';
 
-	return $url;
+    return $url;
 }
 add_filter( 'attachment_link', 'toolbox_enhanced_image_navigation' );
 
